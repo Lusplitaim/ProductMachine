439f4ee9753dda9404acdf1a52f12a60b34eab3c
Lusplitaim <Foolman.ru@yandex.ru>
2024-09-08 19:06:20 +0300

    icon to remove product from basket

--- web/src/app/components/basket/Basket.tsx
+++ web/src/app/components/basket/Basket.tsx
@@ -4,6 +4,7 @@ import NumberInput from "../number-input/NumberInput";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import { useImmer } from "use-immer";
+import { FaRegTrashAlt } from "react-icons/fa";
 
 export default function Basket() {
     const [products, setProducts] = useImmer([] as Product[]);
@@ -26,6 +27,14 @@ export default function Basket() {
         };
     }
 
+    function removeProduct(id: number) {
+        return () => {
+            setProducts(prev => {
+                return prev.filter(p => p.id !== id);
+            });
+        };
+    }
+
     const productsTableData = products.map(p => {
         return (
             <tr key={p.id}>
@@ -34,7 +43,7 @@ export default function Basket() {
                         <figure className="image">
                             <Image src="/coca-cola.jpeg" width={100} height={150} alt="image" />
                         </figure>
-                        <h3>{p.name}</h3>
+                        <h3 className="is-size-4">{p.name}</h3>
                     </div>
                 </td>
                 <td>
@@ -46,6 +55,11 @@ export default function Basket() {
                     <p className="text-center">
                         {p.price}
                     </p>
+                </td>
+                <td>
+                    <span className="icon">
+                        <button onClick={removeProduct(p.id)}><FaRegTrashAlt /></button>
+                    </span>
                 </td>
             </tr>
         );
@@ -72,33 +86,47 @@ export default function Basket() {
         navigate('/');
     }
 
+    const content = products.length
+        ? (
+            <>
+                <table className="table container">
+                    <thead>
+                        <tr>
+                            <th>Товар</th>
+                            <th>Количество</th>
+                            <th>Цена</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {productsTableData}
+                    </tbody>
+                </table>
+                <hr />
+                <div className="flex flex-col h-24 container">
+                    <h3 className="self-end">Итоговая сумма: {getTotalSum()}</h3>
+                    <div className="flex">
+                        <button className="button is-warning is-medium" onClick={navigateBack}>Вернуться</button>
+                        <div className="mx-auto"></div>
+                        <button className="button is-success is-medium" disabled={getTotalSum() <= 0} onClick={navigateToPayment}>Оплата</button>
+                    </div>
+                </div>
+            </>
+        )
+        : (
+            <div className="container flex flex-col items-center justify-center">
+                <p className="is-size-4">У вас нет ни одного товара, вернитесь на страницу каталога</p>
+                <button className="button is-warning is-medium" onClick={navigateBack}>Вернуться</button>
+            </div>
+        );
+
     return (
         <div className="flex flex-col overflow-auto">
             <section className="section flex flex-col">
                 <h1 className="title">Оформление заказа</h1>
             </section>
             <hr />
-            <table className="table container">
-                <thead>
-                    <tr>
-                        <th>Товар</th>
-                        <th>Количество</th>
-                        <th>Цена</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {productsTableData}
-                </tbody>
-            </table>
-            <hr />
-            <div className="flex flex-col h-24 container">
-                <h3 className="self-end">Итоговая сумма: {getTotalSum()}</h3>
-                <div className="flex">
-                    <button className="button is-warning is-medium" onClick={navigateBack}>Вернуться</button>
-                    <div className="mx-auto"></div>
-                    <button className="button is-success is-medium" disabled={getTotalSum() <= 0} onClick={navigateToPayment}>Оплата</button>
-                </div>
-            </div>
+            {content}
         </div>
     );
 }
